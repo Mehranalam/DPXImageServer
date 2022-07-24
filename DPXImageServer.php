180e2bad2c15ada0768effcba4d4b0543f764d01
Mehranalam <mehranalambeigi@gmail.com>
2022-07-24 13:47:42 +0430

    Update

--- DPXImageServer.php
+++ DPXImageServer.php
@@ -9,18 +9,18 @@ defined('DPXImageServer_Storage') or define('DPXImageServer_Storage', __DIR__ . 
 defined('DPXImageServer_TTL') or define('DPXImageServer_TTL', 365 * 86400);
 defined('DPXImageServer_PNGQuant') or define('DPXImageServer_PNGQuant', "pngquant");
 
-function DPXServeImage(string $file_path, int $width = null, int $height = null, int $quality = 80, bool $output_image = true, bool $cache = true) {
+function DPXServeImage(string $file_path, int $width = null, int $height = null, int $quality = 80, bool $output_image = true, bool $cache = true)
+{
 
     if (file_exists($file_path)) {
 
         $mime = strtolower(mime_content_type($file_path));
 
-        if (in_array($mime, [ 'image/png', 'image/jpg', 'image/jpeg' ])) {
+        if (in_array($mime, ['image/png', 'image/jpg', 'image/jpeg'])) {
 
             if (!(is_dir(DPXImageServer_Storage))) {
 
                 @mkdir(DPXImageServer_Storage);
-    
             }
 
             $image = null;
@@ -32,27 +32,22 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                 if (file_exists(DPXImageServer_Storage . "/$filename") && ((int)filemtime(DPXImageServer_Storage . "/$filename") > (time() - DPXImageServer_TTL))) {
 
                     $image = file_get_contents(DPXImageServer_Storage . "/$filename");
-
                 }
-
             }
 
             if (!($image)) {
 
-                [ $original_width, $original_height ] = getimagesize($file_path);
+                [$original_width, $original_height] = getimagesize($file_path);
 
                 if (!($height && $width)) {
 
                     if ($height) {
 
                         $width = ceil($height * $original_width / $original_height);
-
                     } else if ($width) {
 
                         $height = ceil($width * $original_height / $original_width);
-
                     }
-
                 }
 
                 if ($mime === 'image/png') {
@@ -67,11 +62,9 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                         imagepng($image, DPXImageServer_Storage . "/$filename");
 
                         $image = shell_exec("pngquant --quality=" . ($quality - 10) . "-$quality - < " . escapeshellarg(DPXImageServer_Storage . "/$filename"));
-
-                    }else{
+                    } else {
 
                         $image = shell_exec("pngquant --quality=" . ($quality - 10) . "-$quality - < " . escapeshellarg($file_path));
-                        
                     }
 
                     if (!($image)) {
@@ -79,19 +72,14 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                         if (file_exists(DPXImageServer_Storage . "/$filename")) {
 
                             $image = file_get_contents(DPXImageServer_Storage . "/$filename");
-
-                        }else{
+                        } else {
 
                             $image = file_get_contents($file_path);
-
                         }
-
                     }
 
                     @unlink(DPXImageServer_Storage . "/$filename");
-
-
-                }else if ($mime === 'image/jpg' || $mime === 'image/jpeg') {
+                } else if ($mime === 'image/jpg' || $mime === 'image/jpeg') {
 
                     if ($width || $height) {
 
@@ -99,26 +87,21 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                         $original_image = imagecreatefromjpeg($file_path);
                         imagecopyresampled($image, $original_image, 0, 0, 0, 0, $width, $height, $original_width, $original_height);
                         imagedestroy($original_image);
-
-                    }else{
+                    } else {
 
                         $image = imagecreatefromjpeg($file_path);
-
                     }
 
                     ob_start();
                     imagejpeg($image, null, $quality);
                     $image = ob_get_contents();
                     ob_end_clean();
-
                 }
 
                 if ($cache) {
 
                     file_put_contents(DPXImageServer_Storage . "/$filename", $image);
-
                 }
-
             }
 
             if ($output_image) {
@@ -126,17 +109,12 @@ function DPXServeImage(string $file_path, int $width = null, int $height = null,
                 header("Content-Type: $mime");
                 header("Content-Length: " . strlen($image));
                 die($image);
-
-            }else{
+            } else {
 
                 return $image;
-
             }
-
         }
-       
     }
 
     return false;
-
 }
--- README.md
+++ README.md
@@ -1,32 +1,39 @@
-# DPXImageServer
+<p align="center">
+    <a href="https://github.com/Developix-ir">
+        <img src="Assets/RO.svg" alt="Pyrogram" width="128">
+    </a>
+    <br>
+    <b>Small Library to Serve Images in PHP</b>
+    <br>
+    <a href="https://ronio.ir">
+        Homepage
+    </a>
+    •
+    <a href="https://mehranalam.github.io/DPXImageServer/">
+        Documentation
+    </a>
+    •
+    <a href="LICENSE">
+        License
+    </a>
+</p>
 
-Small Library to Serve Images in PHP in a Better Way (Resize, Compress) with Caching Support (:
+### DPXImageServer
 
-Just include/require the library, there are 3 options you could define :  
-  
-`DPXImageServer_Storage` : Path to save files and Keep Caches (insure that it is writable)  
-  
-`DPXImageServer_TTL` : Amount of time that a file cache is valid (In Seconds), Set it to 0 to disable caching (ofcourse if you lost your mind ! why would you disable cache ?)  
-  
-`DPXImageServer_PNGQuant` : Path of PNGQuant Binary, for PNG Compression we use libpngquant and has to be installed on the Server, Otherwise the uncompressed image would be returned  
-  
-  
-There is only a function `DPXServeImage` with these parameters :  
+> Small Library to Serve Images in PHP in a Better Way (Resize, Compress) with Caching Support.
 
-`file_path` : path of the image file to be served (Required | String)  
-  
-`width` : Desired width of image (Optional | Integer)  
-  
-`height` : Desired height of image (Optional | Integer)    
-  
-`quality` : Desired quality of image, a Number between 10-100 (Optional | Integer)
-  
-`output_image` : Wether the image would be outputted or just return the string of the image  
-  
-`cache` : Wether to cache the image or not  
-  
-if Image file you have given is not found or an Invalid format, `DPXServerImage` would return false
+```PHP
+                            ├─ $width ├─ $quality ├─ $cache
+DPXServeImage("image.png", 512, 256, 80, true, true);
+                └─ $file_path    └─ $height └─ $output_image
+```
 
-also keep in mind that if you pass only one of width or height params, the other one will be calculated respectivly, maintaining aspect ratio
+A library for serving images and optimizing, changing their sizes, this library uses the caching feature, and in addition, it is very fast and small in size. In addition to these features, this library also optimizes images.
 
-That's all I can tell right now, But I'll try to document it well later in a better way (also you are welcome to do it too)
+### Key Features
+
+- **Low volume**: This library is very small compared to other examples and is summarized in a .php file.
+- **Optimal**: This library is very optimal due to its small size and consumes very few resources to perform its operations.
+- **Fast**: It has a very high and acceptable speed for performing operations, i.e. changing the size of images, volume of operations and caching operations.
+- **Cache capability**: In computing, a cache is a high-speed data storage layer which stores a subset of data, typically transient in nature, so that future requests for that data are served up faster than is possible by accessing the data’s primary storage location. Caching allows you to efficiently reuse previously retrieved or computed data.
+- **The ability to optimize and reduce the size of images**: In addition to changing the length and width of the photos, this library has the ability to optimize them, just like [smusher](https://smusher.ir/about)
